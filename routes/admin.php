d4b9bd17ec0ad2f37aec731d4bcd957b0c1a9fb2
putul <putulsentongchngya7@gmail.com>
2023-10-17 10:42:10 +0600

    Initial commit

---
+++ routes/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Backend\AdminController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\ProfileController;
+
+// Admin routes
+Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+
+// profile rourte
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
