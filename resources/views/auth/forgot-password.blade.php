00c434e2cd729e7f6309f52cfa65b2d5804ab262
putul <putulsentongchngya7@gmail.com>
2023-10-21 16:23:57 +0600

    dynamic reset password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -47,7 +47,7 @@
                             </form>
 
                         </div>
-                        <a class="see_btn mt-4" href="login.html">go to login</a>
+                        <a class="see_btn mt-4" href="{{route('login')}}">go to login</a>
                     </div>
                 </div>
             </div>
