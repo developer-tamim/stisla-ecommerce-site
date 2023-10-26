6a37259d94c72a6eb2a8df13ca1353a1ede9e7c5
putul <putulsentongchngya7@gmail.com>
2023-10-26 11:51:44 +0600

    user dashboard is dynamic

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,9 +20,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -35,3 +38,8 @@ require __DIR__.'/auth.php';
 // admin routes
 Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
 
+
+
+Route::group(['middleware' => ['auth' , 'verified'], 'prefix'=> 'user', 'as' => 'user.'], function () {
+    Route::get('/dashboard',[UserController::class, 'index'])->name('dashboard');
+});
