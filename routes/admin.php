73704a1f36713e75c440ca8e037f784e90386626
developer-tamim <tawsiftamim24@gmail.com>
2023-11-02 17:08:20 +0600

    slider page set

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Backend\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 
 // Admin routes
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
@@ -12,3 +13,6 @@ Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard'
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+// slider route
+Route::resource('slider', SliderController::class);
