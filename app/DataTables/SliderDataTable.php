55e6a9c9830ad44c5c5ff7f702587663d28434c0
developer-tamim <tawsiftamim24@gmail.com>
2023-11-20 15:50:28 +0600

    complete dynamic slider

--- app/DataTables/SliderDataTable.php
+++ app/DataTables/SliderDataTable.php
@@ -32,11 +32,21 @@ class SliderDataTable extends DataTable
             ->addColumn('banner', function($query){
                 return $img = "<img width='100px' src='".asset($query->banner)."'></img>";
             })
+            ->addColumn('status', function($query){
+                $active = '<i class="badge badge-success">Active</i>';
+                $inActive = '<i class="badge badge-danger">Inactive</i>';
+
+                if ($query->status == 1){
+                    return $active;
+                }else{
+                    return $inActive;
+                }
+            })
 
             // ->addColumn('banner', function($query){
             //     return $img = "<img weidth='100px' src='".asset($query)."'></img>";
             // })
-            ->rawColumns(['banner' , 'action'])
+            ->rawColumns(['banner' , 'action' , 'status' ,])
             ->setRowId('id');
     }
 
@@ -80,6 +90,8 @@ class SliderDataTable extends DataTable
             Column::make('id')->width(100),
             Column::make('banner')->width(200),
             Column::make('title'),
+            Column::make('serial'),
+            Column::make('status'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
