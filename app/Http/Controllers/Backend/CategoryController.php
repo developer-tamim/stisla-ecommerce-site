422fffb178ba1c20da0a08dbf3d0c35ec79eb09c
developer-tamim <tawsiftamim24@gmail.com>
2023-11-30 14:30:53 +0600

    handelling delete button

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -100,6 +100,9 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return response(['status' => 'success' , 'Deleted successfully!']);
     }
 }
