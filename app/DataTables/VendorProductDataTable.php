7e2e8a4514c99261f9d56f676eb90d672a2def84
developer-tamim <tawsiftamim24@gmail.com>
2024-01-04 16:24:33 +0600

    vendor product index

--- app/DataTables/VendorProductDataTable.php
+++ app/DataTables/VendorProductDataTable.php
@@ -22,7 +22,62 @@ class VendorProductDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'vendorproduct.action')
+             // ->addColumn('action', 'product.action')
+             ->addColumn('action', function ($query) {
+                $editBtn = "<a href='" . route('vendor.product.edit', $query->id) . "' class='btn btn-primary d-inline'><i class='fa-regular fa-pen-to-square'></i>Edit</a>";
+                $deleteBtn = "<a href='" . route('vendor.product.destroy', $query->id) . "' class='btn btn-danger d-inline delete-item'><i class='fa-regular fa-trash-can'></i> Delete</a>";
+                $moreBtn = '<div class="dropdown dropleft d-inline">
+                <button class="btn btn-primary dropdown-toggle ml-1" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fa-solid fa-gear"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item has-icon" href="'.route('admin.product-image-gallery.index', ['product' => $query->id]).'"><i class="far fa-heart"></i> Image Gallery</a>
+                  <a class="dropdown-item has-icon" href="'.route('admin.product-variant.index').'"><i class="far fa-file"></i> Variants</a>
+                  <a class="dropdown-item has-icon" href="#"><i class="far fa-clock"></i> Something else here</a>
+                </div>
+              </div>';
+
+                return $editBtn . $deleteBtn . $moreBtn;
+            })
+            ->addColumn('image', function ($query) {
+                return $img = "<img width='70px' src='" . asset($query->thumb_image) . "'></img>";
+            })
+            ->addColumn('type', function ($query) {
+                switch ($query->product_type) {
+                    case 'new_arrival':
+                        return '<i class="badge bg-success">New Arrival</i>';
+                        break;
+                    case 'featured_product':
+                        return '<i class="badge bg-warning">Featured Product</i>';
+                        break;
+                    case 'top_product':
+                        return '<i class="badge bg-info">Top Project</i>';
+                        break;
+                    case 'best_product':
+                        return '<i class="badge bg-danger">Best Product</i>';
+                        break;
+
+                    default:
+                        return '<i class="badge bg-dark">None</i>';
+                        break;
+                }
+            })
+            ->addColumn('status', function ($query) {
+                if ($query->status == 1) {
+                    $button = '<div class="form-check form-switch mt-2">
+                    <input class="form-check-input change-status" checked type="checkbox" id="flexSwitchCheckDefault" data-id="' . $query->id . '">
+                  </div>';
+
+                } else {
+                    $button = '<div class="form-check form-switch mt-2">
+                    <input class="form-check-input change-status" checked type="checkbox" id="flexSwitchCheckDefault" data-id="' . $query->id . '">
+                  </div>';
+
+                }
+
+                return $button;
+            })
+            ->rawColumns(['image', 'type', 'status', 'action'])
             ->setRowId('id');
     }
 
@@ -62,15 +117,19 @@ class VendorProductDataTable extends DataTable
     public function getColumns(): array
     {
         return [
-            Column::computed('action')
-                  ->exportable(false)
-                  ->printable(false)
-                  ->width(60)
-                  ->addClass('text-center'),
             Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+            Column::make('image')->width(150),
+            Column::make('name'),
+            Column::make('price'),
+            Column::make('type')->width(150),
+            Column::make('status'),
+            Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->width(200)
+                ->addClass('text-center'),
+            // Column::make('created_at'),
+            // Column::make('updated_at'),
         ];
     }
 
--- app/Http/Controllers/Backend/vendorProductController.php
+++ app/Http/Controllers/Backend/vendorProductController.php
@@ -11,8 +11,8 @@ use App\Models\Product;
 use App\Models\SubCategory;
 use App\Traits\ImageUploadTraits;
 use Illuminate\Http\Request;
-use Str;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class vendorProductController extends Controller
 {
@@ -101,7 +101,21 @@ class vendorProductController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $subCategories = SubCategory::where('category_id', $product->category_id)->get();;
+        $childCategories = ChildCategory::where('sub_category_id', $product->sub_category_id)->get();;
+        $categories = Category::all();
+        $brands = Brand::all();
+        return view(
+            'vendor.product.edit',
+            compact(
+                'product',
+                'categories',
+                'brands',
+                'subCategories',
+                'childCategories'
+            )
+        );
     }
 
     /**
@@ -109,7 +123,53 @@ class vendorProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+         // dd($request->all());
+         $request->validate([
+            'image' => ['nullable', 'image', 'max:3000'],
+            'name' => ['required', 'max:200'],
+            'category' => ['required'],
+            'brand' => ['required'],
+            'price' => ['required'],
+            'qty' => ['required'],
+            'short_description' => ['required', 'max:600'],
+            'long_description' => ['required'],
+            'seo_title' => ['nullable', 'max:200'],
+            'seo_description' => ['nullable', 'max:2500'],
+            'status' => ['required'],
+        ]);
+
+        $product = Product::findOrFail($id);
+
+        // handle image upload
+        $imagePath = $this->updateImage($request, 'image', 'uploads', $product->thumb_image);
+
+        $product->thumb_image = empty(!$imagePath) ? $imagePath : $product->thumb_image;
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->vendor_id = Auth::user()->vendor->id;
+        $product->category_id = $request->category;
+        $product->sub_category_id = $request->sub_category;
+        $product->child_category_id = $request->child_category;
+        $product->brand_id = $request->brand;
+        $product->qty = $request->qty;
+        $product->short_description = $request->short_description;
+        $product->long_description = $request->long_description;
+        $product->video_url = $request->video_link;
+        $product->sku = $request->sku;
+        $product->price = $request->price;
+        $product->offer_price = $request->offer_price;
+        $product->offer_start_date = $request->offer_start_date;
+        $product->offer_end_date = $request->offer_end_date;
+        $product->product_type = $request->product_type;
+        $product->status = $request->status;
+        $product->is_apporved = $product->is_apporved;
+        $product->seo_title = $request->seo_title;
+        $product->seo_description = $request->seo_description;
+
+        $product->save();
+
+        toastr('Updated Successfully!' , 'success');
+        return redirect()->route('vendor.product.index');
     }
 
     /**
